47f8bd9786222a60e5eb7237ce4782ebb0b32c46
Luis Alejandro Ramirez <lramirezsuarez@gmail.com>
2017-06-15 11:22:24 -0500

    initial commit with base files

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Luis Ramirez on 6/15/17.
+//  Copyright © 2017 Lucho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
